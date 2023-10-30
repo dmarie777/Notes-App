cc0c0d6e152584320d74c997923f12e284d0a4ff
dmarie777 <deimarie777@gmail.com>
2023-10-30 17:39:48 -0500

    Added error handling and unknownEndpoint middleware

--- NotesBackend/index.js
+++ NotesBackend/index.js
@@ -4,9 +4,9 @@ const app = express()
 const cors = require('cors')
 const Note = require('./models/note')
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
 
 
   app.get('/', (request, response) => {
@@ -19,10 +19,16 @@ app.use(express.static('dist'))
     })
   })
 
-  app.get('/api/notes/:id', (request, response ) => {
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
+  app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+      .then(note => {
+        if(note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        } 
     })
+    .catch(error => next(error) )
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -47,6 +53,24 @@ app.use(express.static('dist'))
       response.json(savedNote)
     })
   })
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next)  => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+  }
+
+  app.use(errorHandler)
 
   const PORT = process.env.PORT
   app.listen(PORT, () => {
