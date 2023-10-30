162b47f30c88b3ded1bad5d75281ea4452a4aec9
dmarie777 <deimarie777@gmail.com>
2023-10-29 23:34:16 -0500

    Implemented connection to the database in the backend

---
+++ NotesBackend/models/note.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connect to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB', error.message)
+    })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+const Note = mongoose.model('Note', noteSchema)
+      
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject._v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
