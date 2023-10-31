4802924301b5bfb63db89852529de81eb33dda5c
dmarie777 <deimarie777@gmail.com>
2023-10-31 14:47:17 -0500

    Refactored delete functionality and added put functionality

--- NotesBackend/index.js
+++ NotesBackend/index.js
@@ -32,9 +32,11 @@ app.use(cors())
   })
 
   app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+    Note.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
   })
 
   app.post('/api/notes', (request, response) => {
@@ -53,6 +55,21 @@ app.use(cors())
       response.json(savedNote)
     })
   })
+
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
+
 
   const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
