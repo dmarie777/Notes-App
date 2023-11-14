f29645c4e1bd3daac0453f8e83fe2856f3e5f84c
dmarie777 <deimarie777@gmail.com>
2023-11-14 14:19:31 -0500

    Added the functionality to add data to the database in a controlled manner for the tests

--- NotesBackend/mongo.js
+++ NotesBackend/mongo.js
@@ -6,7 +6,9 @@ if (process.argv.length<3) {
 }
 const password = process.argv[2]
 
-const url = `mongodb+srv://deys:${password}@cluster0.lhtlhhq.mongodb.net/noteApp?retryWrites=true&w=majority`
+// const url = `mongodb+srv://deys:${password}@cluster0.lhtlhhq.mongodb.net/noteApp?retryWrites=true&w=majority`
+// url for test///
+const url = `mongodb+srv://deys:${password}@cluster0.hn793gc.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
@@ -18,19 +20,26 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
+// const note1 = new Note({
 //   content: 'HTML is Easy',
 //   important: true,
 // })
-// note.save().then(result => {
+// note1.save().then(result => {
 //   console.log('note saved!')
-//   mongoose.connection.close()
 // })
 
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
+const note2 = new Note({
+  content: 'Browser can execute only JavaScript',
+  important: true,
+})
+note2.save().then(() => {
   mongoose.connection.close()
 })
+
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
 
